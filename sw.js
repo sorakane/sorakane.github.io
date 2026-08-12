8c08b57dec166e15d95f8bc621360e3c6de55790
sorakane <61083745+sorakane@users.noreply.github.com>
2026-08-12 19:44:16 +0900

    Keep name card visible while dragging

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "enbu-formation-editor-v5";
+const CACHE_NAME = "enbu-formation-editor-v6";
 const APP_SHELL = ["/", "/manifest.webmanifest", "/favicon.svg"];
 
 self.addEventListener("install", (event) => {
