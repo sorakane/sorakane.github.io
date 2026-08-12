dc8d157b6883e1147b56e788521c1928700e7d08
sorakane <61083745+sorakane@users.noreply.github.com>
2026-08-12 19:52:42 +0900

    Highlight and follow active formation tab

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "enbu-formation-editor-v6";
+const CACHE_NAME = "enbu-formation-editor-v7";
 const APP_SHELL = ["/", "/manifest.webmanifest", "/favicon.svg"];
 
 self.addEventListener("install", (event) => {
