7519fef69b283098f6cb9d26989b31479fdf32fe
sorakane <61083745+sorakane@users.noreply.github.com>
2026-08-12 18:56:28 +0900

    Swap members on occupied positions

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "enbu-formation-editor-v4";
+const CACHE_NAME = "enbu-formation-editor-v5";
 const APP_SHELL = ["/", "/manifest.webmanifest", "/favicon.svg"];
 
 self.addEventListener("install", (event) => {
