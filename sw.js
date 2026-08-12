88e6dc79ec194f12a481ab0cefa340a5e439427c
sorakane <61083745+sorakane@users.noreply.github.com>
2026-08-12 14:48:20 +0900

    Clear retired saved member data

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "enbu-formation-editor-v1";
+const CACHE_NAME = "enbu-formation-editor-v3";
 const APP_SHELL = ["/", "/manifest.webmanifest", "/favicon.svg"];
 
 self.addEventListener("install", (event) => {
