3f39672b83d6bf1a021a9fde133a925e079191f5
Enbu Formation Editor <noreply@users.noreply.github.com>
2026-08-17 09:55:40 +0900

    Refine editor information hierarchy

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "enbu-formation-editor-v54";
+const CACHE_NAME = "enbu-formation-editor-v55";
 const APP_SHELL = ["/", "/manifest.webmanifest", "/favicon.svg"];
 
 self.addEventListener("install", (event) => {
