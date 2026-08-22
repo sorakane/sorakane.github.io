27b6a741e03179457862c99c6733bc3f488a3932
Enbu Formation Editor <noreply@users.noreply.github.com>
2026-08-22 14:30:50 +0900

    Add dark mode to formation editor

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "enbu-formation-editor-v59";
+const CACHE_NAME = "enbu-formation-editor-v60";
 const APP_SHELL = ["/", "/manifest.webmanifest", "/favicon.svg"];
 
 self.addEventListener("install", (event) => {
