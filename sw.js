013117c850e3b54c9687887b1f3852554b950521
Enbu Formation Editor <noreply@users.noreply.github.com>
2026-09-05 20:34:19 +0900

    Replace overflowing formation action icons

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "enbu-formation-editor-v63";
+const CACHE_NAME = "enbu-formation-editor-v64";
 const APP_SHELL = ["/", "/manifest.webmanifest", "/favicon.svg"];
 
 self.addEventListener("install", (event) => {
