3073ec7b40e79f2111dd2d68ba092b64eec672be
Enbu Formation Editor <noreply@users.noreply.github.com>
2026-09-06 08:24:20 +0900

    Add social link preview metadata

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "enbu-formation-editor-v66";
+const CACHE_NAME = "enbu-formation-editor-v67";
 const APP_SHELL = ["/", "/manifest.webmanifest", "/favicon.svg"];
 
 self.addEventListener("install", (event) => {
