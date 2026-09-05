f2b6f324f4adc25b8996e11438a0d6b0800fc9ca
Enbu Formation Editor <noreply@users.noreply.github.com>
2026-09-05 21:01:58 +0900

    Allow hiding entrance and exit zones

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "enbu-formation-editor-v65";
+const CACHE_NAME = "enbu-formation-editor-v66";
 const APP_SHELL = ["/", "/manifest.webmanifest", "/favicon.svg"];
 
 self.addEventListener("install", (event) => {
