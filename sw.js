3e1c360f64e65c6dfca631a4821a8d3baf303044
Enbu Formation Editor <noreply@users.noreply.github.com>
2026-09-06 16:36:36 +0900

    Add new project storage choices

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "enbu-formation-editor-v70";
+const CACHE_NAME = "enbu-formation-editor-v71";
 const APP_SHELL = ["/", "/manifest.webmanifest", "/favicon.svg"];
 
 self.addEventListener("install", (event) => {
