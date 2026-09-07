8458498da410ca768ec08ff9aa5978fee9ad23d0
Enbu Formation Editor <noreply@users.noreply.github.com>
2026-09-08 01:12:16 +0900

    Propagate parade number adjustments to following formations

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "enbu-formation-editor-v79";
+const CACHE_NAME = "enbu-formation-editor-v80";
 const APP_SHELL = ["/", "/manifest.webmanifest", "/favicon.svg"];
 
 self.addEventListener("install", (event) => {
