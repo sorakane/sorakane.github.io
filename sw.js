2e279ac0d4e7df6354f1d59d4f129a3b3e9924e5
Enbu Formation Editor <noreply@users.noreply.github.com>
2026-09-07 22:21:39 +0900

    Allow dragging formation tabs to reorder

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "enbu-formation-editor-v73";
+const CACHE_NAME = "enbu-formation-editor-v74";
 const APP_SHELL = ["/", "/manifest.webmanifest", "/favicon.svg"];
 
 self.addEventListener("install", (event) => {
