80bb4c7347d6a4d626bbae25712c4ea93ca173e1
Enbu Formation Editor <noreply@users.noreply.github.com>
2026-09-12 11:14:35 +0900

    Make member rename save and completion explicit

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "enbu-formation-editor-v89";
+const CACHE_NAME = "enbu-formation-editor-v90";
 const APP_SHELL = ["/", "/manifest.webmanifest", "/favicon.svg"];
 
 self.addEventListener("install", (event) => {
