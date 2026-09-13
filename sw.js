d0b7d749c3100ed4603d6040149d01501c5a7d93
Enbu Formation Editor <noreply@users.noreply.github.com>
2026-09-13 20:21:58 +0900

    Support mouse wheel navigation in fullscreen slide review

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "enbu-formation-editor-v91";
+const CACHE_NAME = "enbu-formation-editor-v92";
 const APP_SHELL = ["/", "/manifest.webmanifest", "/favicon.svg"];
 
 self.addEventListener("install", (event) => {
