97836f622cf970025740c68c96287eaa907a0d06
Enbu Formation Editor <noreply@users.noreply.github.com>
2026-09-13 19:53:09 +0900

    Remove misleading global backup counter and download reminder

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "enbu-formation-editor-v90";
+const CACHE_NAME = "enbu-formation-editor-v91";
 const APP_SHELL = ["/", "/manifest.webmanifest", "/favicon.svg"];
 
 self.addEventListener("install", (event) => {
